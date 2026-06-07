efdf7e4ed28e027a71be1a07129b6e98f42f8316
CorranHornet <texhex14@outlook.com>
2026-06-07 17:14:49 +0200

    feat(infrastructure): add ApplicationDbContext and infrastructure setup

---
+++ InfrastructureLayer/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿
+using ApplicationLayer.Interfaces;
+using InfrastructureLayer.Database;
+using InfrastructureLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TournamentApi.Infrastructure.Repositories;
+
+
+namespace InfrastructureLayer
+{
+    public static class DependencyInjection
+    {
+        //Här registeras alla beroende / Paket /Services från Applikation 
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Registrera alla Services, beroenden, paket osv...
+            //DB och Repositories
+            services.AddDbContext<AppDbContext>(options =>
+            {
+            options.UseSqlServer(configuration.GetConnectionString("NemoCleanArchitectureDbString"));
+            });
+                
+
+            services.AddScoped<IGameRepository, GameRepository>();
+            services.AddScoped<ITournamentRepository, TournamentRepository>();
+            return services;
+        }
+    }
+}
---
+++ InfrastructureLayer/Repositories/TournamentRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ApplicationLayer.Interfaces;
+using DomainLayer.Models;
+using InfrastructureLayer.Database;
+
+namespace InfrastructureLayer.Repositories
+{
+    public class TournamentRepository : ITournamentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TournamentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Tournament>> GetAllAsync()
+            => await _context.Tournaments
+                .Include(t => t.Games)
+                .ToListAsync();
+
+        public async Task<Tournament?> GetByIdAsync(int id)
+            => await _context.Tournaments
+                .Include(t => t.Games)
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+        public async Task AddAsync(Tournament tournament)
+            => await _context.Tournaments.AddAsync(tournament);
+
+        public async Task SaveAsync()
+            => await _context.SaveChangesAsync();
+
+        public async Task DeleteAsync(Tournament tournament)
+        {
+            _context.Tournaments.Remove(tournament);
+            await Task.CompletedTask;
+        }
+    }
+}
